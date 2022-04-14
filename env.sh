60746fd49a2dcacd557f1cfe3bf07955739043b9
Sharath Sahadevan <sharath.sahadevan@mendix.com>
2022-04-14 15:58:35 -0500

    Create env.sh
    Added env.sh for aws keys

---
+++ env.sh
@@ -0,0 +1,9 @@
+cd ~
+. ./env.sh
+cd -
+export AWS_ACCESS_KEY_ID="TODO:your-aws-access-key"
+export AWS_SECRET_ACCESS_KEY="TODO:"
+export AWS_DEFAULT_REGION="TODO:"
+
+# Dec 9 , 2021 Added for Load Balancer Config
+export LBC_VERSION="v2.3.0"
