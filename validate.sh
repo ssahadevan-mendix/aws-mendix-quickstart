3ebc5c8066947ea66c0c59a65545b74a157eed6e
Sharath Sahadevan <sharath.sahadevan@mendix.com>
2022-04-27 11:59:26 -0500

    Update validate.sh

--- validate.sh
+++ validate.sh
@@ -10,7 +10,7 @@ podsRunning=$(kubectl get pods --all-namespaces | grep Running | wc -l)
 
 if [[ $podsRunning -ge $expectedResult ]];
 then 
-  echo "Result: "
+  echo "Result: Count of pods running "
   echo "$0 - Success: Number of running pods is " $podsRunning 
   echo ""
 else
@@ -23,8 +23,15 @@ demoPodsExpected=1
 demoPodsRunning=$(kubectl get pods --all-namespaces | grep -i demo-master |  grep Running | wc -l)
 if [[ $demoPodsRunning -eq $demoPodsExpected ]];
 then 
-  echo "Result: "
+  echo "Result: Checking Application Pods "
   echo "$0 - Success: Number of running demo application pods is " $demoPodsRunning 
+  echo ""
+  appUrl=$(fgrep -i appUrl demo.yaml | cut -f2 -d':' )
+  echo "$0   Application Url:"$appUrl
+  # kubectl get svc -n grafana | grep -i LoadBalancer
+  prometheusAddress=$(kubectl get svc -n grafana  | grep -i LoadBalancer | awk '{ print $4 }' )
+  prometheusPort=$(kubectl get svc -n grafana| grep -i LoadBalancer | awk '{ print $5 }' | cut -f1 -d':')
+  echo "$0   Prometheus Url: " $prometheusAddress":"$prometheusPort
   echo ""
 else
   echo "Result:*** "
